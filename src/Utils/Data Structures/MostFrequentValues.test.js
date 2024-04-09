043dcb24bd0bbfc5098c8aabe44f1f8d2b1c27bb
Sruthi Sanala <dineshsanala4@gmail.com>
2024-04-09 13:53:17 +0530

    test cases added for most frequent algo

--- src/Utils/Data Structures/MostFrequentValues.test.js
+++ src/Utils/Data Structures/MostFrequentValues.test.js
@@ -4,3 +4,8 @@ test("get top 2 most frequent values of - [1, 2, 3, 2, 1, 2, 4, 5, 4, 2]", () =>
   const arr = [1, 2, 3, 2, 1, 2, 4, 5, 4, 2];
   expect(getFrequentValues(arr, 2)).toEqual([2, 1]);
 });
+
+test("get top 4 most frequent values of - [1, 2, 3, 2, 1, 2, 4, 5, 4, 2]", () => {
+  const arr = [1, 2, 3, 2, 1, 2, 4, 5, 4, 2];
+  expect(getFrequentValues(arr, 4)).toEqual([2, 1, 4, 3]);
+});

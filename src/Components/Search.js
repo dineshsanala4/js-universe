b2c0f8e590c8eff3a9c2240f5a9686cdd12c7e30
Sruthi Sanala <dineshsanala4@gmail.com>
2024-04-28 14:37:51 +0530

    removed unused import statement

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import useDebounce from "../Hooks/useDebounce";
 
 function Search() {

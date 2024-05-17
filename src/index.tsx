faa738c1cd1a7af4082182491f777643a7be151a
Sruthi Sanala <dineshsanala4@gmail.com>
2024-05-17 15:56:29 +0530

    console log removed

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-//Test Comment
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
